e97a4e93a1c951af028d7d010b0bd59d9ebe4b8c
Mathias Arlaud <mathias.arlaud@gmail.com>
2021-04-29 13:23:38 +0200

    Add "bundle friendly" persistence service aliases

--- src/Resources/config/storage/doctrine.php
+++ src/Resources/config/storage/doctrine.php
@@ -17,39 +17,39 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $container): void {
     $container->services()
 
-        ->set(ClientManager::class)
+        ->set('league.oauth2_server.manager.doctrine.client', ClientManager::class)
             ->args([
                 null,
             ])
-        ->alias(ClientManagerInterface::class, ClientManager::class)
-        ->alias('league.oauth2_server.manager.doctrine.client', ClientManager::class)
+        ->alias(ClientManagerInterface::class, 'league.oauth2_server.manager.doctrine.client')
+        ->alias(ClientManager::class, 'league.oauth2_server.manager.doctrine.client')
 
-        ->set(AccessTokenManager::class)
+        ->set('league.oauth2_server.manager.doctrine.access_token', AccessTokenManager::class)
             ->args([
                 null,
             ])
-        ->alias(AccessTokenManagerInterface::class, AccessTokenManager::class)
-        ->alias('league.oauth2_server.manager.doctrine.access_token', AccessTokenManager::class)
+        ->alias(AccessTokenManagerInterface::class, 'league.oauth2_server.manager.doctrine.access_token')
+        ->alias(AccessTokenManager::class, 'league.oauth2_server.manager.doctrine.access_token')
 
-        ->set(RefreshTokenManager::class)
+        ->set('league.oauth2_server.manager.doctrine.refresh_token', RefreshTokenManager::class)
             ->args([
                 null,
             ])
-        ->alias(RefreshTokenManagerInterface::class, RefreshTokenManager::class)
-        ->alias('league.oauth2_server.manager.doctrine.refresh_token', RefreshTokenManager::class)
+        ->alias(RefreshTokenManagerInterface::class, 'league.oauth2_server.manager.doctrine.refresh_token')
+        ->alias(RefreshTokenManager::class, 'league.oauth2_server.manager.doctrine.refresh_token')
 
-        ->set(AuthorizationCodeManager::class)
+        ->set('league.oauth2_server.manager.doctrine.authorization_code', AuthorizationCodeManager::class)
             ->args([
                 null,
             ])
-        ->alias(AuthorizationCodeManagerInterface::class, AuthorizationCodeManager::class)
-        ->alias('league.oauth2_server.manager.doctrine.authorization_code', AuthorizationCodeManager::class)
+        ->alias(AuthorizationCodeManagerInterface::class, 'league.oauth2_server.manager.doctrine.authorization_code')
+        ->alias(AuthorizationCodeManager::class, 'league.oauth2_server.manager.doctrine.authorization_code')
 
-        ->set(DoctrineCredentialsRevoker::class)
+        ->set('league.oauth2_server.credentials_revoker.doctrine', DoctrineCredentialsRevoker::class)
             ->args([
                 null,
             ])
-        ->alias(CredentialsRevokerInterface::class, DoctrineCredentialsRevoker::class)
-        ->alias('league.oauth2_server.credentials_revoker.doctrine', DoctrineCredentialsRevoker::class)
+        ->alias(CredentialsRevokerInterface::class, 'league.oauth2_server.credentials_revoker.doctrine')
+        ->alias(DoctrineCredentialsRevoker::class, 'league.oauth2_server.credentials_revoker.doctrine')
     ;
 };
