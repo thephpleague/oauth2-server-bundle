c9ca7a9159efc1dcfb3c4547011bc0a87ba01b73
TLG-Gildas <129415070+TLG-Gildas@users.noreply.github.com>
2026-06-16 15:01:20 +0000

    fix: add trigger deprecation on deprecated usage of GrantTypeInterface

--- src/AuthorizationServer/GrantConfigurator.php
+++ src/AuthorizationServer/GrantConfigurator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace League\Bundle\OAuth2ServerBundle\AuthorizationServer;
 
 use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Grant\GrantTypeInterface as LeagueGrantTypeInterface;
 
 /**
  * @deprecated
@@ -28,6 +29,14 @@ final class GrantConfigurator
     {
         foreach ($this->grants as $grant) {
             if ($grant instanceof GrantTypeInterface) {
+                trigger_deprecation('league/oauth2-server-bundle', '1.2', '%s implements custom grant using %s interface which is deprecated. Use %s interface and tag %s with accessTokenTTL attribute instead. See : %s',
+                    $grant::class,
+                    GrantTypeInterface::class,
+                    LeagueGrantTypeInterface::class,
+                    'league.oauth2_server.authorization_server.grant',
+                    'https://github.com/thephpleague/oauth2-server-bundle/blob/v1.2.0/docs/implementing-custom-grant-type.md'
+                );
+
                 $authorizationServer->enableGrantType($grant, $grant->getAccessTokenTTL());
             }
         }
