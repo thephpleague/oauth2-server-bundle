thephpleague/oauth2-server-bundle

commit 4945bc5721e06d0a659329cfa76217402c188b73

Allow setting extra claims to the access token

src/Repository/AccessTokenRepository.php
@@ -6,14 +6,17 @@ namespace League\Bundle\OAuth2ServerBundle\Repository;
 
 use League\Bundle\OAuth2ServerBundle\Converter\ScopeConverterInterface;
 use League\Bundle\OAuth2ServerBundle\Entity\AccessToken as AccessTokenEntity;
+use League\Bundle\OAuth2ServerBundle\Event\AccessTokenExtraClaimsResolveEvent;
 use League\Bundle\OAuth2ServerBundle\Manager\AccessTokenManagerInterface;
 use League\Bundle\OAuth2ServerBundle\Manager\ClientManagerInterface;
 use League\Bundle\OAuth2ServerBundle\Model\AbstractClient;
 use League\Bundle\OAuth2ServerBundle\Model\AccessToken as AccessTokenModel;
+use League\Bundle\OAuth2ServerBundle\OAuth2Events;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Exception\UniqueTokenIdentifierConstraintViolationException;
 use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 final class AccessTokenRepository implements AccessTokenRepositoryInterface
 {
@@ -32,19 +35,31 @@ final class AccessTokenRepository implements AccessTokenRepositoryInterface
      */
     private $scopeConverter;
 
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
     public function __construct(
         AccessTokenManagerInterface $accessTokenManager,
         ClientManagerInterface $clientManager,
         ScopeConverterInterface $scopeConverter,
+        EventDispatcherInterface $eventDispatcher,
     ) {
         $this->accessTokenManager = $accessTokenManager;
         $this->clientManager = $clientManager;
         $this->scopeConverter = $scopeConverter;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function getNewToken(ClientEntityInterface $clientEntity, array $scopes, ?string $userIdentifier = null): AccessTokenEntityInterface
     {
-        $accessToken = new AccessTokenEntity();
+        $event = $this->eventDispatcher->dispatch(
+            new AccessTokenExtraClaimsResolveEvent($clientEntity, $scopes, $userIdentifier),
+            OAuth2Events::ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE
+        );
+
+        $accessToken = new AccessTokenEntity($event->getExtraClaims());
         $accessToken->setClient($clientEntity);
         if (null !== $userIdentifier && '' !== $userIdentifier) {
             $accessToken->setUserIdentifier($userIdentifier);

config/access_token/default.php
@@ -10,6 +10,7 @@ use League\Bundle\OAuth2ServerBundle\Manager\ClientManagerInterface;
 use League\Bundle\OAuth2ServerBundle\Repository\AccessTokenRepository;
 use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 return static function (ContainerConfigurator $container): void {
     $container->services()
@@ -19,6 +20,7 @@ return static function (ContainerConfigurator $container): void {
                 service(AccessTokenManagerInterface::class),
                 service(ClientManagerInterface::class),
                 service(ScopeConverterInterface::class),
+                service(EventDispatcherInterface::class),
             ])
         ->alias(AccessTokenRepositoryInterface::class, 'league.oauth2_server.repository.access_token')
         ->alias(AccessTokenRepository::class, 'league.oauth2_server.repository.access_token');

src/Entity/AccessToken.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace League\Bundle\OAuth2ServerBundle\Entity;
 
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\ClaimsFormatter;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
@@ -11,7 +13,54 @@ use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
 
 final class AccessToken implements AccessTokenEntityInterface
 {
-    use AccessTokenTrait;
+    use AccessTokenTrait {
+        initJwtConfiguration as private traitInitJwtConfiguration;
+    }
     use EntityTrait;
     use TokenEntityTrait;
+
+    /**
+     * @param array<non-empty-string, mixed> $extraClaims
+     */
+    public function __construct(
+        private array $extraClaims = [],
+    ) {
+    }
+
+    public function initJwtConfiguration(): void
+    {
+        $this->traitInitJwtConfiguration();
+        $builder = $this->jwtConfiguration->builder();
+
+        if (!method_exists($this->jwtConfiguration, 'withBuilderFactory')) { // @phpstan-ignore function.alreadyNarrowedType
+            $this->jwtConfiguration->setBuilderFactory(
+                $this->createBuilderFactory($builder, $this->extraClaims)
+            );
+
+            return;
+        }
+
+        $this->jwtConfiguration = $this->jwtConfiguration->withBuilderFactory(
+            $this->createBuilderFactory($builder, $this->extraClaims)
+        );
+    }
+
+    /**
+     * @param array<non-empty-string, mixed> $extraClaims
+     *
+     * @return \Closure(ClaimsFormatter): Builder
+     */
+    private function createBuilderFactory(Builder $builder, array $extraClaims): \Closure
+    {
+        return static function (ClaimsFormatter $claimFormatter) use ($builder, $extraClaims): Builder {
+            foreach ($extraClaims as $name => $value) {
+                if ('scopes' === $name) {
+                    throw new \InvalidArgumentException('The "scopes" claim is reserved and cannot be used as an extra claim.');
+                }
+                $builder = $builder->withClaim($name, $value);
+            }
+
+            return $builder;
+        };
+    }
 }

src/Event/AccessTokenExtraClaimsResolveEvent.php
@@ -1,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace League\Bundle\OAuth2ServerBundle\Event;
+
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class AccessTokenExtraClaimsResolveEvent extends Event
+{
+    /**
+     * @var array<non-empty-string,mixed>
+     */
+    private array $extraClaims = [];
+
+    /**
+     * @param ScopeEntityInterface[] $scopes
+     */
+    public function __construct(
+        private ClientEntityInterface $clientEntity,
+        private array $scopes,
+        private ?string $userIdentifier = null,
+    ) {
+    }
+
+    public function getClient(): ClientEntityInterface
+    {
+        return $this->clientEntity;
+    }
+
+    /**
+     * @return ScopeEntityInterface[]
+     */
+    public function getScopes(): array
+    {
+        return $this->scopes;
+    }
+
+    public function getUserIdentifier(): ?string
+    {
+        return $this->userIdentifier;
+    }
+
+    /**
+     * @param array<non-empty-string, mixed> $extraClaims
+     */
+    public function setExtraClaims(array $extraClaims): self
+    {
+        $this->extraClaims = $extraClaims;
+
+        return $this;
+    }
+
+    /**
+     * @return array<non-empty-string, mixed>
+     */
+    public function getExtraClaims(): array
+    {
+        return $this->extraClaims;
+    }
+}

src/OAuth2Events.php
@@ -46,4 +46,11 @@ final class OAuth2Events
      * You could alter the client here.
      */
     public const PRE_SAVE_CLIENT = 'league.oauth2_server.event.pre_save_client';
+
+    /**
+     * The ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE event occurs right before the access token is issued.
+     *
+     * You could add extra claims to the access token here.
+     */
+    public const ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE = 'league.oauth2_server.event.access_token_extra_claims_resolve';
 }

tests/Acceptance/AuthorizationEndpointTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace League\Bundle\OAuth2ServerBundle\Tests\Acceptance;
 
+use League\Bundle\OAuth2ServerBundle\Event\AccessTokenExtraClaimsResolveEvent;
 use League\Bundle\OAuth2ServerBundle\Event\AuthorizationRequestResolveEvent;
 use League\Bundle\OAuth2ServerBundle\Manager\AccessTokenManagerInterface;
 use League\Bundle\OAuth2ServerBundle\Manager\AuthorizationCodeManagerInterface;
@@ -387,6 +388,44 @@ final class AuthorizationEndpointTest extends AbstractAcceptanceTest
         $this->assertArrayHasKey('expires_in', $fragment);
         $this->assertArrayHasKey('state', $fragment);
         $this->assertEquals('foobar', $fragment['state']);
+    }
+
+    public function testSuccessfulTokenWithExtraClaimsRequest(): void
+    {
+        $eventDispatcher = $this->client
+            ->getContainer()
+            ->get('event_dispatcher');
+        $eventDispatcher->addListener(OAuth2Events::AUTHORIZATION_REQUEST_RESOLVE, static function (AuthorizationRequestResolveEvent $event): void {
+            $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_APPROVED);
+        });
+        $eventDispatcher->addListener(OAuth2Events::ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE, static function (AccessTokenExtraClaimsResolveEvent $event): void {
+            $event->setExtraClaims(['foo' => 'bar', 'baz' => ['qux', 'quux']]);
+        });
+
+        $this->loginUser();
+
+        $this->client->request(
+            'GET',
+            '/authorize',
+            [
+                'client_id' => FixtureFactory::FIXTURE_CLIENT_FIRST,
+                'response_type' => 'token',
+                'state' => 'foobar',
+            ]
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertSame(302, $response->getStatusCode());
+        $redirectUri = $response->headers->get('Location');
+
+        $this->assertStringStartsWith(FixtureFactory::FIXTURE_CLIENT_FIRST_REDIRECT_URI, $redirectUri);
+        $fragment = [];
+        parse_str(parse_url($redirectUri, \PHP_URL_FRAGMENT), $fragment);
+        $this->assertArrayHasKey('access_token', $fragment);
+        $tokenParts = TestHelper::parseJwtToken($fragment['access_token']);
+        $this->assertSame($tokenParts['payload']['foo'], 'bar');
+        $this->assertSame($tokenParts['payload']['baz'], ['qux', 'quux']);
     }
 
     public function testCodeRequestRedirectToResolutionUri(): void

tests/Integration/AbstractIntegrationTest.php
@@ -123,7 +123,7 @@ abstract class AbstractIntegrationTest extends TestCase
         $scopeConverter = new ScopeConverter();
         $scopeRepository = new ScopeRepository($this->scopeManager, $this->clientManager, $scopeConverter, $this->eventDispatcher);
         $clientRepository = new ClientRepository($this->clientManager);
-        $accessTokenRepository = new AccessTokenRepository($this->accessTokenManager, $this->clientManager, $scopeConverter);
+        $accessTokenRepository = new AccessTokenRepository($this->accessTokenManager, $this->clientManager, $scopeConverter, $this->eventDispatcher);
         $refreshTokenRepository = new RefreshTokenRepository($this->refreshTokenManager, $this->accessTokenManager);
         $userConverter = new UserConverter();
         $userRepository = new UserRepository($this->clientManager, $this->eventDispatcher, $userConverter);

tests/TestHelper.php
@@ -106,4 +106,23 @@ final class TestHelper
 
         return 0 === $statusCode;
     }
+
+    /**
+     * @return array{header: array<non-empty-string, string>, payload: array<non-empty-string, mixed>, signature: non-empty-string}
+     */
+    public static function parseJwtToken(string $jwt): array
+    {
+        $parts = explode('.', $jwt);
+        if (3 !== \count($parts)) {
+            throw new \InvalidArgumentException('Invalid JWT token format.');
+        }
+
+        [$header, $payload, $signature] = $parts;
+
+        return [
+            'header' => json_decode(base64_decode($header), true),
+            'payload' => json_decode(base64_decode($payload), true),
+            'signature' => $signature,
+        ];
+    }
 }

commit b44db9e7632e1d1ac840d1c88f3bbd2caef6a2e9

Fix BC break
It was introduced in 4945bc5721e06d0a659329cfa76217402c188b73

src/Repository/AccessTokenRepository.php
@@ -36,7 +36,7 @@ final class AccessTokenRepository implements AccessTokenRepositoryInterface
     private $scopeConverter;
 
     /**
-     * @var EventDispatcherInterface
+     * @var EventDispatcherInterface|null
      */
     private $eventDispatcher;
 
@@ -44,22 +44,35 @@ final class AccessTokenRepository implements AccessTokenRepositoryInterface
         AccessTokenManagerInterface $accessTokenManager,
         ClientManagerInterface $clientManager,
         ScopeConverterInterface $scopeConverter,
-        EventDispatcherInterface $eventDispatcher,
+        ?EventDispatcherInterface $eventDispatcher = null,
     ) {
         $this->accessTokenManager = $accessTokenManager;
         $this->clientManager = $clientManager;
         $this->scopeConverter = $scopeConverter;
+        if (null === $eventDispatcher) {
+            trigger_deprecation(
+                'league/oauth2-server-bundle',
+                '1.2',
+                'The "%s" class constructor will require a new "%s $eventDispatcher" argument in 2.0. Not passing it is deprecated',
+                self::class,
+                EventDispatcherInterface::class
+            );
+        }
         $this->eventDispatcher = $eventDispatcher;
     }
 
     public function getNewToken(ClientEntityInterface $clientEntity, array $scopes, ?string $userIdentifier = null): AccessTokenEntityInterface
     {
-        $event = $this->eventDispatcher->dispatch(
-            new AccessTokenExtraClaimsResolveEvent($clientEntity, $scopes, $userIdentifier),
-            OAuth2Events::ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE
-        );
+        if (null !== $this->eventDispatcher) {
+            $event = $this->eventDispatcher->dispatch(
+                new AccessTokenExtraClaimsResolveEvent($clientEntity, $scopes, $userIdentifier),
+                OAuth2Events::ACCESS_TOKEN_EXTRA_CLAIMS_RESOLVE
+            );
+        } else {
+            $event = null;
+        }
 
-        $accessToken = new AccessTokenEntity($event->getExtraClaims());
+        $accessToken = new AccessTokenEntity($event?->getExtraClaims() ?? []);
         $accessToken->setClient($clientEntity);
         if (null !== $userIdentifier && '' !== $userIdentifier) {
             $accessToken->setUserIdentifier($userIdentifier);