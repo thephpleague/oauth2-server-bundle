d2a6b9f2a11e158daba350d990af3efa6b444bc0
Daniel Blok <dblok@123inkt.nl>
2026-06-30 10:55:20 +0200

    Implement audience restriction in AccessToken class

--- src/Entity/AccessToken.php
+++ src/Entity/AccessToken.php
@@ -7,17 +7,20 @@ namespace League\Bundle\OAuth2ServerBundle\Entity;
 use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\ClaimsFormatter;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
+use League\OAuth2\Server\Entities\AudienceRestrictedTokenInterface;
 use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
+use League\OAuth2\Server\Entities\Traits\AudienceRestrictedTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
 
-final class AccessToken implements AccessTokenEntityInterface
+final class AccessToken implements AccessTokenEntityInterface, AudienceRestrictedTokenInterface
 {
     use AccessTokenTrait {
         initJwtConfiguration as private traitInitJwtConfiguration;
     }
     use EntityTrait;
     use TokenEntityTrait;
+    use AudienceRestrictedTokenTrait;
 
     /**
      * @param array<non-empty-string, mixed> $extraClaims
